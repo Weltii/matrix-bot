32b49ccc5d0129a2221734b8778c8c2fbc5f0387
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2020-02-17 00:36:54 +0100

    Add bot command processing

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
+from app.nio_client import BotClient
+from nio import (AsyncClient, SyncResponse, RoomMessageText, LoginResponse)
+import asyncio
+from importlib import util
 import sys
 sys.path.append("./")
 
-from importlib import util
-import asyncio
-from nio import (AsyncClient, SyncResponse, RoomMessageText, LoginResponse)
-from app.nio_client import BotClient
-
 
 bot: BotClient = BotClient(
-	"https://matrix.org",
-	"weltiimore",
-	"JmvAkL4fEDN4NhXUNAWY8Scwvt37by6Gw9zPu6d4rDEV9ASxCe85sLkG6pCXCSgj"
+    "https://matrix.org",
+    "weltiimore",
+    "JmvAkL4fEDN4NhXUNAWY8Scwvt37by6Gw9zPu6d4rDEV9ASxCe85sLkG6pCXCSgj"
 )
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(bot.run_client())
 loop.close()
+print("Bot are offline now! Goodby 👋")
--- app/nio_client.py
+++ app/nio_client.py
@@ -1,51 +1,64 @@
-from importlib import util
-import asyncio
+from .utils import (get_last_batch, save_batch)
 from nio import (AsyncClient, SyncResponse, RoomMessageText, LoginResponse)
-from utils import (get_last_batch, save_batch)
-
-response_string = "!mensa"
+import asyncio
+from importlib import util
+import sys
+sys.path.append("./")
 
 
 class BotClient(AsyncClient):
+    def shutdown(self):
+        self.run = False
+        return "I will shut me down!"
+
+    def process_input(self, input: str):
+        output = "I don't have any idea what I can do with this command {}!"\
+            .format(input)
+        input.strip()
+        if input.startswith("!"):
+            if input.startswith("!bot"):
+                command = input.replace("!bot", "").strip()
+                func = self.command_mapping.get(command)
+                if func:
+                    output = func()
+        return output
+
     async def run_sync_loop(self):
-        while(True):
+        while(self.run):
             sync_response: SyncResponse = await self.sync(30000)
             save_batch(sync_response.next_batch)
             if (len(sync_response.rooms.join) > 0):
                 for room_id in sync_response.rooms.join:
                     for event in sync_response.rooms.join[room_id].timeline.events:
-                        if hasattr(event, "body") and \
-                                event.body.startswith(response_string) and \
-                                event.sender != self.user_id:
-                            response_body = event.body.replace(
-                                response_string, "").strip()
+                        if hasattr(event, "body") and event.sender != self.user_id:
                             content = {
-                                "body": response_body,
+                                "body":
+                                self.process_input(event.body),
                                 "msgtype": "m.text"
                             }
                             await self.room_send(room_id, 'm.room.message', content)
-                        elif event.sender != self.user_id:
-                            response_body = event.body.replace(
-                                response_string, "").strip()
-                            content = {
-                                "body": 'I don\'t understand {}!'.format(response_body),
-                                "msgtype": "m.text"
-                            }
-                            await self.room_send(room_id, 'm.room.message', content)
-
+        print("end of sync loop!")
 
     async def login_client(self, password):
         login_response = await self.login(password)
-        print(login_response) # add something when he failed!
+        print(login_response)  # add something when he failed!
         self.user_id = login_response.user_id
         self.next_batch = get_last_batch()
 
+    async def logout_client(self):
+        await self.logout()
+        print("logout sucessfull!")
 
     async def run_client(self):
         await self.login_client(self.password)
         await self.run_sync_loop()
-
+        await self.logout()
+        await self.close()
 
     def __init__(self, home_server: str, user_name: str, password: str):
         AsyncClient.__init__(self, home_server, user_name)
         self.password = password
+        self.run = True
+        self.command_mapping = dict(
+            shutdown=self.shutdown
+        )
